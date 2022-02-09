1a50d91c25f97f692c971f931f036be991ff0d6a
DCT <mirachan2332@gmail.com>
2022-02-08 18:57:04 -0600

    validation added

--- server.js
+++ server.js
@@ -73,17 +73,35 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-  // req.body is where our incoming content will be
+  // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  const animal = createNewAnimal(req.body, animals);
-
-  console.log(req.body);
-  res.json(animal);
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
 
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
 //CONTINUE AT 11.2.6: Add Validation to Our Data
